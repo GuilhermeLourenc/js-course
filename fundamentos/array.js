caa46c434e1bb0a16c08792b257905b1418a9755
GuilhermeLourenc <guii.glourenco@hotmail.com>
2019-02-16 22:06:16 -0200

    js-foundation

---
+++ fundamentos/array.js
@@ -0,0 +1,3 @@
+const valores = [7,8,9,6,5];
+
+console.log(valores[0], valores[3]);
---
+++ fundamentos/objetos.js
@@ -0,0 +1,13 @@
+// Objeto é uma coleção de chave, valores.
+
+const prod1 = {}
+prod1.nome = 'Celular Ultra Mega';
+
+
+const prod2 = {
+    nome : 'Camisa Polo',
+    valor: 79.90
+}
+
+console.log(prod2);
+
