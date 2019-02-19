5daaf24ebf51c18e783b36dc02e4483d2abc0096
GuilhermeLourenc <guii.glourenco@hotmail.com>
2019-02-19 01:25:31 -0300

    foundation-js

--- fundamentos/tempCodeRunnerFile.js
+++ fundamentos/tempCodeRunnerFile.js
@@ -1,4 +1,7 @@
-for (let i = 0; i<10;i++ ) {
-   console.log(i)
-}
+console.log(typeof Object);
+console.log(typeof new Object);
+
+const Cliente = function () {}
+console.log(typeof Cliente) 
+console.log(typeof new Cliente)
 
